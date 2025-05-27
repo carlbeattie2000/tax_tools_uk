7ec81ca924bc3e660618956eee0631d40ac1508f
carlbeattie2000 <12beattiecastp@gmail.com>
2025-05-27 11:45:32 +0100

    fix(logging): ignore logging files

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -13,7 +13,7 @@ var (
 
 func GetLogger() *log.Logger {
 	once.Do(func() {
-		file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		file, err := os.OpenFile("info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Fatalf("Failed to open log file: %v", err)
 		}
