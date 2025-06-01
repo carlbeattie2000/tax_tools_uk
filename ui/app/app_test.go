6ae9e6a3443801a11217e9cd8490a8a50d44b7ab
carlbeattie2000 <12beattiecastp@gmail.com>
2025-06-01 01:46:24 +0100

    add(app): tests

---
+++ ui/app/app_test.go
@@ -0,0 +1,66 @@
+package app
+
+import (
+	"errors"
+	"tax_calculator/engine/lib/router"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAppHasDefault404Handler(t *testing.T) {
+	app := NewApplication()
+	done := make(chan struct{})
+
+	go func() {
+		app.Start()
+		close(done)
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	res := app.Fetch("/", nil, "")
+
+	app.QueueUpdate(func() {
+		app.Stop()
+	})
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		t.Fatal("App did not stop in time")
+	}
+
+	assert.Equal(t, 404, res.Status)
+}
+
+func TestAppHasDefaultErrorHandler(t *testing.T) {
+	app := NewApplication()
+	done := make(chan struct{})
+
+	go func() {
+		app.Get("/", func(req *router.Request, res *router.Response, next router.NextFunc) {
+			next(errors.New("oh a error"))
+		})
+
+		app.Start()
+		close(done)
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	res := app.Fetch("/", nil, "")
+
+	app.QueueUpdate(func() {
+		app.Stop()
+	})
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		t.Fatal("App did not stop in time")
+	}
+
+	assert.Equal(t, 500, res.Status)
+}
